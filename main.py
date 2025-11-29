887ba97a0414ced523e259578f1567e0ac96bad6
meshcher <meshcher.06@mail.ru>
2025-11-29 17:05:42 +0300

    Bird вынесен в отдельный файл

--- main.py
+++ main.py
@@ -4,29 +4,11 @@ from PyQt6.QtCore import Qt, QTimer
 from PyQt6.QtGui import QColor, QPainter
 from PyQt6.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QWidget
 
+from bird import Bird
+
 # Константы
 SCREEN_WIDTH = 400
 SCREEN_HEIGHT = 600
-
-
-class Bird:
-    def __init__(self, x, y, size=30, color="#FFD700"):
-        self.x = x
-        self.y = y
-        self.size = size
-        self.color = QColor(color)
-
-        self.velocity = 0
-        self.gravity = 0.5
-
-    def move(self):
-        self.velocity += self.gravity
-        self.y += self.velocity
-
-    def draw(self, painter):
-        painter.setBrush(self.color)
-        painter.setPen(Qt.PenStyle.NoPen)
-        painter.drawEllipse(int(self.x), int(self.y), self.size, self.size)
 
 
 # ИГРОВАЯ ОБЛАСТЬ
